f2d3d2204854edc0e8a3c13d2c617b7fab35607d
Mohsen Mirshekari <79303215+mohsenmir@users.noreply.github.com>
2021-11-14 20:31:07 +0330

    Login and JWT implemented

--- module/routes/api/api-v1.js
+++ module/routes/api/api-v1.js
@@ -3,13 +3,15 @@ const router = express.Router();
 const mongoose = require('mongoose');
 const User = require('../../models/user');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 
 router.get('/',(req,res) => {
     res.json('Welcome to Api')
 });
 
 // Register
-router.post('/Register',(req,res,next)=>{
+router.post('/register',(req,res,next)=>{
     User.find({email:req.body.email})
     .exec()
     .then(user => {
@@ -53,6 +55,52 @@ router.post('/Register',(req,res,next)=>{
     })
     .catch();
 });
+
+
+// Login
+router.post('/login',(req,res,next)=>{
+    User.find({ email: req.body.email})
+    .exec()
+    .then(user => {
+        if(user.length < 1){
+            return res.status(401).json({
+                message: 'اطلاعات وارد شده صحیح نیست'
+            });
+        }
+        bcrypt.compare(req.body.password, user[0].password, (err,result) => {
+            if(err){
+                return res.status(401).json({
+                    message: 'اطلاعات وارد شده صحیح نیست'
+                });
+            }
+            if(result) {
+            const token = jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, '1p09@d#d)g95ivgy&eih#8z82h02mqvl5czx2#0i&)4!prvqt=',
+                {
+                    expiresIn: '1h', // توکن بعد از یک ساعت غیرفعال میشه
+                },
+                
+                )
+                return res.status(200).json({
+                    message : 'با موفقیت وارد شدید',
+                    token : token
+                });
+            }
+            return res.status(401).json({
+                message: 'اطلاعات وارد شده صحیح نیست'
+            });
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error : err
+        });
+    }); 
+})
+
 
 // Delete User
 router.delete('/:userId' , (req , res , next) => {
